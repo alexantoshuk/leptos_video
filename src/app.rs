75c09e423f9f5aaf6ca795a44b171530b952fff0
alexantoshuk <alexander.antoshuk@gmail.com>
2025-11-10 13:48:23 +0300

    refactor: meve service-worker register script to shell

--- src/app.rs
+++ src/app.rs
@@ -16,6 +16,26 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <AutoReload options=options.clone() />
                 <HydrationScripts options />
+
+                <script>
+                    r#"
+                    if ('serviceWorker' in navigator) {
+                        window.addEventListener('load', () => {
+                            navigator.serviceWorker.register('/service-worker.js')
+                            .then(registration => {
+                                console.log('SW registered: ', registration);
+                            })
+                            .catch(registrationError => {
+                                console.log('SW registration failed: ', registrationError);
+                            });
+                        });
+                    }
+                    "#
+                </script>
+
+                <Stylesheet id="leptos" href="/pkg/app.css" />
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+                <link rel="manifest" href="/manifest.json" />
                 <MetaTags />
             </head>
             <body>
@@ -31,28 +51,6 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <script>
-            r#"
-            if ('serviceWorker' in navigator) {
-                window.addEventListener('load', () => {
-                    navigator.serviceWorker.register('/service-worker.js')
-                    .then(registration => {
-                        console.log('SW registered: ', registration);
-                    })
-                    .catch(registrationError => {
-                        console.log('SW registration failed: ', registrationError);
-                    });
-                });
-            }
-            "#
-        </script>
-
-        <Stylesheet id="leptos" href="/pkg/app.css" />
-
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
-
-        <link rel="manifest" href="/manifest.json" />
-
         // sets the document title
         <Title text="Welcome  to Leptos" />
 
